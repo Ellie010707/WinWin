09af4ca42e1db569358a2773ad32d739b6429b69
Ellie <doris0707@naver.com>
2023-05-11 10:32:58 +0900

    sol: b15651 N과 M (3)

---
+++ 3주차/b15651.py
@@ -0,0 +1,14 @@
+N, M = map(int, input().split())
+
+l = []
+
+def dfs():
+    if len(l) == M:
+        print(" ".join(map(str, l)))
+        return
+    for i in range(1, N+1):
+        l.append(i)
+        dfs()
+        l.pop()
+
+dfs()
